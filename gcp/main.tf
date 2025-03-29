1373b789bfe842089a4f4a1caa18238818847ce2
Kaustav Talukdar <talukdark555@gmail.com>
2025-03-29 22:06:39 +0530

    updated google credentials

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "test_bucket" {
-    name = "terraform-test-bucket"
+    name = "terraform-test-bucket-kaustav"
     location = var.region
     storage_class = "STANDARD"
     project = var.project
--- gcp/provider.tf
+++ gcp/provider.tf
@@ -1,4 +1,5 @@
 provider "google" {
+    impersonate_service_account = "jenkins-sa@solo-levelling-arise.iam.gserviceaccount.com"
     project = var.project
     region = var.region
 }
