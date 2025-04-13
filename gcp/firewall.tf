310f8ffe4f50e780b23b3b74f47015870acf17fb
Kaustav Talukdar <talukdark555@gmail.com>
2025-04-13 17:45:13 +0530

    updated FW rule

--- gcp/firewall.tf
+++ gcp/firewall.tf
@@ -6,6 +6,18 @@ resource "google_compute_firewall" "allow-iap" {
     protocol = "tcp"
     ports    = ["22"]
   }
-  source_ranges = ["35.235.240.0/20"]
+  source_ranges = ["35.235.240.0/20","10.128.0.0/16"]
   target_tags = ["allow-iap"]
 }
+
+/*resource "google_compute_firewall" "allow-access-from-laptop" {
+  name    = "allow-ingress-from-laptop"
+  network = google_compute_network.my_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+  source_ranges = ["103.24.126.206/32"]
+  target_tags = ["browser-access"]
+}*/
